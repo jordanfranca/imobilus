ff0fec3c509b92a5c81f991ff91c78a255c7c52f
Jordã França <jordan1992_16@hotmail.com>
2014-08-24 00:29:02 -0300

    Metodos da controller home para controller cadastros

--- controllers/cadastro.controller.php
+++ controllers/cadastro.controller.php
@@ -8,6 +8,28 @@
 		public function error() {
 			require("views/erro/erro.view.php");
 		}
+		
+		public function add() {
+			$objCadastro = new Cadastro();
+			$objCadastro->setIntCreci($_POST['creci']);
+			$objCadastro->setStrEmail($_POST['email']);
+			$objCadastro->setStrNome($_POST['nome']);
+			$objCadastro->setStrLogin(Helpers::sha512($_POST['creci'] . $_POST['login']));
+			$objCadastro->setStrSenha(Helpers::sha512($_POST['creci'] . $_POST['senha']));
+			$objCadastro->setStrHash($strHash = Helpers::sha512(Helpers::geraSenha()));
+				
+			$objCadastro->Inserir();
+			$objEmail = new Email();
+			$objEmail->confirmaCadastro($_POST['email'], $_POST['nome'], $strHash);
+				
+		}
+		
+		public function consultarId () {
+			$objCadastro = new Cadastro();
+			$objCadastro->setIntCodigo($_POST['id']);
+			$objCadastro->getCadastro();
+		}
+		
 	}
 	
 	//Verifica Metodo na classe
