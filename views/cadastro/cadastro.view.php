3d379a2dc006ef620fc4a9296f087efee745d57f
Jordã França <jordan1992_16@hotmail.com>
2014-08-24 00:44:17 -0300

    Trocando endereço do form

--- views/cadastro/cadastro.view.php
+++ views/cadastro/cadastro.view.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="large-10 large-centered medium-12 medium-uncentered small-12 small-uncentered columns text-center" id="cadastro">
 			<h3>Preencha o formulário e cadastre-se no nosso site!</h3>
-			<form action="/home/add" method="POST">
+			<form action="/cadastro/add" method="POST">
 				<input class="text-field" required type="text" placeholder="Nome" name="nome" /><br />
 				<input class="text-field" required type="text" placeholder="E-mail" name="email" /><br />
 				<input class="text-field" required type="text" placeholder="Login" name="login" /><br />
